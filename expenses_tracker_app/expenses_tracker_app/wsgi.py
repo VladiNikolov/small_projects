e553371178a56dbc921bbeb5f2dda4cc42e90aaf
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2023-12-30 11:17:14 +0200

    Create wsgi.py

---
+++ expenses_tracker_app/expenses_tracker_app/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for expenses_tracker_app project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expenses_tracker_app.settings')
+
+application = get_wsgi_application()
