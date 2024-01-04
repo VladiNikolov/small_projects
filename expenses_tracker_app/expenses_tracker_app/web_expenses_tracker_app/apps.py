01a3cf70571e1885c2a8576eeeda11e7b190c544
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2024-01-04 08:17:45 +0200

    Create apps.py

---
+++ expenses_tracker_app/expenses_tracker_app/web_expenses_tracker_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WebExpensesTrackerAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'expenses_tracker_app.web_expenses_tracker_app'
