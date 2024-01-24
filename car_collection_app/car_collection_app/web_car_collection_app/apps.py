39c7cc0da93be1ea58afe09557b941535e19c079
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2024-01-24 08:20:36 +0200

    Create apps.py

---
+++ car_collection_app/car_collection_app/web_car_collection_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WebCarCollectionAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'car_collection_app.web_car_collection_app'
