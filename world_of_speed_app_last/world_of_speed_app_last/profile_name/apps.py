a12b33f8b4a41f463591cf0790d69a54700f0fef
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2024-04-26 09:21:43 +0300

    Create apps.py

---
+++ world_of_speed_app_last/world_of_speed_app_last/profile_name/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProfileNameConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'world_of_speed_app_last.profile_name'
